e2a0c148699cbec0d9e59bd2ef7450b077fba7d2
Radouan FARAH <redouane.farah.1995@gmail.com>
2021-10-23 01:40:36 +0100

    villes

--- routes/ville_quartier.js
+++ routes/ville_quartier.js
@@ -3,7 +3,7 @@ var pool = require('./../config/db')
 var router = express.Router();
 
 router.get('/getVilles', (req, res) => {
-    pool.query(`select * from villes`, (err, result) => {
+    pool.query(`select id, name, (select count(*) from users where ville=B.name and role="C") as consomateur, (select count(*) from users where ville=B.name and role ="V") as vendeur from villes as B`, (err, result) => {
         if (err) {
             console.log("Erreur dans la récupération des villes : ", err);
             return res.status(500).json({})
